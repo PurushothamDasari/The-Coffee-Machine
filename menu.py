b6c42b7bab851482af26d8dec7a5f2312eac363c
Purushotham Dasari <punith.purushotham.dasari@gmail.com>
2025-07-11 12:22:51 +0530

    text correction

--- menu.py
+++ menu.py
@@ -20,14 +20,14 @@ class Menu:
         ]
 
     def get_items(self):
-        """Returns all the names of the available menu.txt items"""
+        """Returns all the names of the available menu items"""
         options = ""
         for item in self.menu:
             options += f"{item.name}/"
         return options
 
     def find_drink(self, order_name):
-        """Searches the menu.txt for a particular drink by name. Returns that item if it exists, otherwise returns None"""
+        """Searches the menu for a particular drink by name. Returns that item if it exists, otherwise returns None"""
         for item in self.menu:
             if item.name == order_name:
                 return_item = item
